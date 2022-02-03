8bbb28e2dd8b964dd193365d94d549ae44b37cea
Mustafizur Rahman <mustafizur232@gmail.com>
2022-02-04 00:59:40 +0600

    visibility

--- reverse.js
+++ reverse.js
@@ -1,8 +1,11 @@
 const greetings = 'Hello, how are you?';
 function reverseString(text) {
+    let reverse = '';
     for (const letter of text) {
         console.log(letter);
+        reverse = reverse + letter;
     }
+    return reverse;
 }
 const reversed = reverseString(greetings);
 console.log(reversed);
