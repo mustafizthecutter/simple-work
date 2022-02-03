f03274bcdc2c91d12b57829246ae0190d549d5ff
Mustafizur Rahman <mustafizur232@gmail.com>
2022-02-03 21:47:03 +0600

    again commit

--- duplicate.js
+++ duplicate.js
@@ -1,8 +1,16 @@
 const bestFriends = ['ahsan', 'murad', 'hridoy', 'mustafiz', 'ahsan', 'sami', 'sami', 'ammar', 'mustafiz'];
 
-function removeDuplicate() {
-    for (let i = 0; i <= names.length; i++) {
-        const element = names[i];
-        console.log(element);
+function removeDuplicate(bestFriends) {
+    const unique = [];
+    for (let i = 0; i <= bestFriends.length; i++) {
+        var element = bestFriends[i];
+
+        if (unique.indexOf(element) == -1) {
+            unique.push(element);
+        }
+        // console.log(element);
     }
+    return unique;
 }
+const result = removeDuplicate(bestFriends);
+console.log(result);
