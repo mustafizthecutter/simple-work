38f1646a944548f363bf8bbda9c324d28ca3e1c9
Mustafizur Rahman <mustafizur232@gmail.com>
2022-02-03 20:36:57 +0600

    start

---
+++ duplicate.js
@@ -0,0 +1,8 @@
+const bestFriends = ['ahsan', 'murad', 'hridoy', 'mustafiz', 'ahsan', 'sami', 'sami', 'ammar', 'mustafiz'];
+
+function removeDuplicate() {
+    for (let i = 0; i <= names.length; i++) {
+        const element = names[i];
+        console.log(element);
+    }
+}
