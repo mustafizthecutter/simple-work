47146b4b051ee449c284caf49aeeba352b8c371b
Mustafizur Rahman <mustafizur232@gmail.com>
2022-02-03 16:52:23 +0600

    another commit

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,17 @@
-// here [0,1,1,2,3,5,8,13,21,34,55]
-const fibo = [0, 1];
-for (let i = 2; i <= 10; i++) {
-    fibo[i] = fibo[i - 1] + fibo[i - 2];
+// // here [0,1,1,2,3,5,8,13,21,34,55]
+// const fibo = [0, 1];
+// for (let i = 2; (i >= 40 && i <= 49); i++) {
+//     fibo[i] = fibo[i - 1] + fibo[i - 2];
+// }
+// console.log(fibo);
+
+function fibonacciSeries(nums) {
+    const fibo = [0, 1];
+    for (let i = 2; i <= nums; i++) {
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+    }
+    return fibo;
+
 }
-console.log(fibo);
+const getElement = fibonacciSeries(10);
+console.log(getElement);
