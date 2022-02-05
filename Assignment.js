c417a7c162974e9d1ffcb447883cae8e6f348055
Mustafizur Rahman <mustafizur232@gmail.com>
2022-02-06 00:10:35 +0600

    pet baira jaibo

--- Assignment.js
+++ Assignment.js
@@ -2,16 +2,32 @@
 
 // 1 vori = 16 ana
 
-function anaToVori(ana) {
-    if (typeof ana != 'number') {
-        return "Please enter valid a number";
-    }
-    else if (ana <= 0) {
-        return "Please enter a Number";
-    }
-    const vori = ana / 16;
-    return vori;
+// function anaToVori(ana) {
+//     if (typeof ana != 'number') {
+//         return "Please enter valid a number";
+//     }
+//     else if (ana <= 0) {
+//         return "Please enter a Number";
+//     }
+//     const vori = ana / 16;
+//     return vori;
+// }
+// const ana = 5;
+// const result = anaToVori(ana);
+// console.log(result);
+
+
+// Problem No: 02
+
+function pandaCost(SingaraQuantity, SamuchaQuantity, JilapiQuantity) {
+    const SingaraPrice = 7;
+    const SamuchaPrice = 10;
+    const JilapiPrice = 15;
+    const SingaraOrderPrice = SingaraPrice * SingaraQuantity;
+    const SamuchaOrderPrice = SamuchaPrice * SamuchaQuantity;
+    const JilapiOrderPrice = JilapiPrice * JilapiQuantity;
+    const totalOrderPrice = SingaraOrderPrice + SamuchaOrderPrice + JilapiOrderPrice;
+    return totalOrderPrice;
 }
-const ana = 5;
-const result = anaToVori(ana);
+const result = pandaCost(2, 5, 3);
 console.log(result);
