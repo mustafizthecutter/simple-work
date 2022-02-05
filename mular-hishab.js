c150af38e712ce89bebf61394c4ed346af85f7a7
Mustafizur Rahman <mustafizur232@gmail.com>
2022-02-05 14:29:28 +0600

    add again

--- mular-hishab.js
+++ mular-hishab.js
@@ -1,4 +1,13 @@
 function mularDam(mulaQuantity) {
+    if (typeof mulaQuantity != "number") {
+        return "Please enter a Valid Number";
+    }
+    if (mulaQuantity <= 0) {
+        return "Please Enter a Number";
+    }
+    if (mulaQuantity > 50) {
+        return "Please want some less";
+    }
     let price = 0;
     if (mulaQuantity <= 2) {
         price = mulaQuantity * 30;
