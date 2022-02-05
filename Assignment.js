3090f33a90738ca1672ad37c34818a28460a7735
Mustafizur Rahman <mustafizur232@gmail.com>
2022-02-05 22:33:46 +0600

    pushing again

--- Assignment.js
+++ Assignment.js
@@ -3,9 +3,15 @@
 // 1 vori = 16 ana
 
 function anaToVori(ana) {
+    if (typeof ana != 'number') {
+        return "Please enter valid a number";
+    }
+    else if (ana <= 0) {
+        return "Please enter a Number";
+    }
     const vori = ana / 16;
     return vori;
 }
-const ana = 2;
+const ana = 5;
 const result = anaToVori(ana);
 console.log(result);
