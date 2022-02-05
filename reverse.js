09298d8e7c70cb21ebf280dab20c0aed5c447a7f
Mustafizur Rahman <mustafizur232@gmail.com>
2022-02-05 19:29:03 +0600

    kire

--- reverse.js
+++ reverse.js
@@ -1,11 +1,20 @@
-const greetings = 'Hello, how are you?';
-function reverseString(text) {
-    let reverse = '';
+// const greetings = 'Hello, how are you?';
+// function reverseString(text) {
+//     let reverse = '';
+//     for (const letter of text) {
+//         console.log(letter);
+//         reverse = reverse + letter;
+//     }
+//     return reverse;
+// }
+// const reversed = reverseString(greetings);
+// console.log(reversed);
+
+function reversedArray(text) {
     for (const letter of text) {
         console.log(letter);
-        reverse = reverse + letter;
     }
-    return reverse;
+    return letter;
 }
-const reversed = reverseString(greetings);
-console.log(reversed);
+const string = "Mustafizur Rahman";
+console.log();
